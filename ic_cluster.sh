511cdb6c31f24bd6839ff1d90190dee18b53d9cd
Martin Müller <martin.mathias.mueller@gmail.com>
2018-12-14 13:19:42 +0100

    add tmux conf

--- ic_cluster.sh
+++ ic_cluster.sh
@@ -16,6 +16,7 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 rsync ./bash_profile ~/.bash_profile
 rsync ./aliases ~/.aliases
 rsync ./functions ~/.functions
+rsync ./tmux.conf ~/.tmux.conf
 source ~/.bash_profile
 
 # vim
