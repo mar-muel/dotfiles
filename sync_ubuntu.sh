99de84bb725357c2128cde3c9e3bb741d7a857e2
Martin Müller <martin.mathias.mueller@gmail.com>
2019-05-20 20:24:18 +0200

    sync ubuntu script

---
+++ sync_ubuntu.sh
@@ -0,0 +1,5 @@
+rsync --progress ~/.vimrc ubuntu/vimrc
+rsync --progress ~/.bash_aliases ubuntu/bash_aliases
+rsync --progress ~/.gitconfig ubuntu/gitconfig
+rsync --progress ~/.bashrc ubuntu/bashrc
+rsync --progress ~/.tmux.conf ubuntu/tmux.conf
