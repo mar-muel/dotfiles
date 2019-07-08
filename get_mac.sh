96f349259dd09964ed442027b0f8af105b6b9855
Martin Müller <martin.mathias.mueller@gmail.com>
2019-07-08 16:34:41 +0200

    get set scripts

---
+++ get_mac.sh
@@ -0,0 +1,6 @@
+rsync --progress ~/.vimrc mac/vimrc
+rsync --progress ~/.gitconfig mac/gitconfig
+rsync --progress ~/.bashrc mac/bashrc
+rsync --progress ~/.profile mac/profile
+rsync --progress ~/.tmux.conf mac/tmux.conf
+rsync --progress ~/.zshrc mac/zshrc
---
+++ set_ubuntu.sh
@@ -0,0 +1,5 @@
+rsync --progress ubuntu/vimrc ~/.vimrc 
+rsync --progress ubuntu/bash_aliases ~/.bash_aliases 
+rsync --progress ubuntu/gitconfig ~/.gitconfig
+rsync --progress ubuntu/bashrc ~/.bashrc 
+rsync --progress ubuntu/tmux.conf ~/.tmux.conf
