b44838d4c20f464038230cb756af53b6cf78db19
Martin Müller <martin.mathias.mueller@gmail.com>
2019-07-08 16:34:21 +0200

    update ubuntu dotfiles

--- sync_mac.sh
+++
@@ -1,6 +0,0 @@
-rsync --progress ~/.vimrc mac/vimrc
-rsync --progress ~/.gitconfig mac/gitconfig
-rsync --progress ~/.bashrc mac/bashrc
-rsync --progress ~/.profile mac/profile
-rsync --progress ~/.tmux.conf mac/tmux.conf
-rsync --progress ~/.zshrc mac/zshrc
