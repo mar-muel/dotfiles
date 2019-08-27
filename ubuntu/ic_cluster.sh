0d234c7cdf742aacb38417e4d7d1b4bf9effa023
Martin Müller <martin.mathias.mueller@gmail.com>
2019-08-27 15:21:59 +0200

    condarc, ubuntu setup

--- ubuntu/ic_cluster.sh
+++ ubuntu/ic_cluster.sh
@@ -1,24 +1,14 @@
 # Before running: make sure conda and vim are installed
 
-# git config
-rsync ./gitconfig ~/.gitconfig
-
-# conda config
-conda config --add channels conda-forge 
-
 # install stuff
 conda install rsync tmux ncurses ripgrep -y
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-# sync profiles
-rsync ./bash_profile ~/.bash_profile
-rsync ./bash_aliases ~/.bash_aliases
-rsync ./functions ~/.functions
-rsync ./tmux.conf ~/.tmux.conf
-source ~/.bash_profile
+# tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# Then run prefix-I inside tmux
 
 # vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-rsync ./vimrc ~/.vimrc
 vim -c 'PluginInstall' -c 'qa!'
