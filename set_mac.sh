ced556e7cc53600b8986e345c50b23b217b76421
Martin Müller <martin.mathias.mueller@gmail.com>
2020-06-01 12:16:07 +0200

    mac config

---
+++ set_mac.sh
@@ -0,0 +1,7 @@
+rsync --progress mac/vimrc ~/.vimrc
+rsync --progress mac/gitconfig ~/.gitconfig
+rsync --progress mac/profile ~/.profile
+rsync --progress mac/bashrc ~/.bashrc
+rsync --progress mac/zshrc ~/.zshrc
+rsync --progress mac/tmux.conf ~/.tmux.conf
+rsync --progress mac/condarc ~/.condarc
