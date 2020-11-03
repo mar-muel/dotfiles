547ae9b0aae2345bc8ccc1970767ed5290acc86a
Martin Müller <martin.mathias.mueller@gmail.com>
2020-11-03 13:47:29 +0100

    latex compile function

--- set_mac.sh
+++
@@ -1,7 +0,0 @@
-rsync --progress mac/vimrc ~/.vimrc
-rsync --progress mac/gitconfig ~/.gitconfig
-rsync --progress mac/profile ~/.profile
-rsync --progress mac/bashrc ~/.bashrc
-rsync --progress mac/zshrc ~/.zshrc
-rsync --progress mac/tmux.conf ~/.tmux.conf
-rsync --progress mac/condarc ~/.condarc
