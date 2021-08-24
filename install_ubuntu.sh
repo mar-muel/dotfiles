8e3f4d2e04df045470b8242a0ec08d98e2a25e6b
Martin Müller <martin.mathias.mueller@gmail.com>
2021-08-24 15:06:41 +0200

    Create install_ubuntu.sh

---
+++ install_ubuntu.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# good ol update
+sudo apt update && sudo apt upgrade -y
+
+# Install some general tools
+sudo apt-get install -y tmux vim-gtk htop git rsync ripgrep
+
+# Install mar-muel specific dotfiles
+git clone https://github.com/mar-muel/dotfiles.git ~/dotfiles
+cd ~/dotfiles && chmod +x set_ubuntu.sh && bash set_ubuntu.sh
+cd ~ && source ~/.bashrc 
+
+# vim setup
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+# Install miniconda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+bash ~/miniconda.sh -b -p $HOME/miniconda
+$HOME/miniconda/bin/conda init bash
+source ~/.bashrc
+rm ~/miniconda.sh
