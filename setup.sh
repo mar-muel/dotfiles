0ff5b47a9c77467efc70be0ac99f1cdab5dd55a7
Martin Müller <martin.mathias.mueller@gmail.com>
2023-12-06 16:29:02 -0500

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim -c 'PluginInstall'
-
-# miniconda
-mkdir ~/downloads && cd ~/downloads
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh
-cd && source ~/.bashrc
