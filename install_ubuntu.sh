5908857c937c43101fb6f37cdfb6c7d0d6fc5cc5
Martin Muller <martin.muller.me@gmail.com>
2026-01-18 00:12:48 +0100

    update unix setup

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -6,11 +6,6 @@ sudo apt update && sudo apt upgrade -y
 # Install some general tools
 sudo apt-get install -y tmux htop git rsync flake8 jq nmon git-lfs mosh
 
-# Install neovim
-sudo add-apt-repository -y ppa:neovim-ppa/unstable
-sudo apt-get update
-sudo apt-get install -y neovim python3-neovim
-
 # Install ripgrep
 wget https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
 sudo dpkg -i ripgrep_13.0.0_amd64.deb
@@ -20,14 +15,6 @@ rm ripgrep_13.0.0_amd64.deb
 git clone https://github.com/mar-muel/dotfiles.git ~/dotfiles
 cd ~/dotfiles && chmod +x set_ubuntu.sh && bash set_ubuntu.sh
 cd ~ && source ~/.bashrc
-
-# vim setup
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-nvim +PlugInstall +qall
-
-# install Vundle (vim)
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # tmux setup
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && ~/.tmux/plugins/tpm/scripts/install_plugins.sh
